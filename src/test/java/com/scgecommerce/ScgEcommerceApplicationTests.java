6ea103addaf58228bee8da261a61f5ee81a6d084
je-pa <jieun0502@gmail.com>
2024-09-01 02:25:59 +0900

    Initial commit

---
+++ src/test/java/com/scgecommerce/ScgEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scgecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgEcommerceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
